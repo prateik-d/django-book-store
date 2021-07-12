424fa9d37d17cc10e5640cd2acf458089fa508c5
prateik-d <pratik.dhotmal@gmail.com>
2021-07-12 12:52:38 +0530

    data & models

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Book
+from django.http import Http404
+from django.db.models import Avg
 
 # Create your views here.
+
+
+def index(request):
+
+    books = Book.objects.all().order_by("-rating")
+    
+    total_number_of_books = books.count
+
+    average_rating = books.aggregate(Avg("rating"))
+
+    return render(request, "book_outlet/index.html", {
+        "books" : books,
+        "total_number_of_books" : total_number_of_books,
+        "average_rating" : average_rating,
+    })
+
+def book_detail(request, slug):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404()
+
+    book = get_object_or_404(Book, slug=slug)
+
+    return render(request, "book_outlet/book_detail.html", {
+        "titel" : book.title,
+        "author" : book.author,
+        "rating" : book.rating,
+        "is_bestselling" : book.is_bestselling
+    })
